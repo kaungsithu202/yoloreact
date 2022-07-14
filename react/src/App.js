e9f449f71aa1061cf55a48caf40d727f36dc58e2
kaungMyatMinKmm <kaungmyatminkmm1@gmail.com>
2022-07-15 02:59:33 +0630

    add auth register

--- react/src/App.js
+++ react/src/App.js
@@ -10,6 +10,15 @@ import NavbarCo from './components/NavbarCo';
 import Register from './components/Frontend/Auth/Register';
 import Login from './components/Frontend/Auth/Login';
 
+import axios from 'axios';
+
+
+
+axios.defaults.baseURL = "http://localhost:8000/";
+axios.defaults.headers.post['Content-Type'] = 'application/json';
+axios.defaults.headers.post['Accept'] = 'application/json';
+
+axios.defaults.withCredentials = true;
 function App() {
   return (
 		<div className="mw-100">
